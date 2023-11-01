b6a1d2c88314784b76d05815ac7a0996ac6c2d1e
serge.muradian@gmail.com <serge.muradian@gmail.com>
2023-11-01 10:53:52 +0300

    Create Header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import logo from '../assets/Images/logo.png'
+import { HiMoon, HiOutlineMagnifyingGlass, HiSun } from 'react-icons/hi2'
 
 const Header = () => {
+    const [toggle, setToggle] = useState(false);
     return (
-        <div>Header</div>
+        <div className="flex items-center p-3">
+            <img src={logo} width={60} height={60}/>
+            <div className="flex bg-slate-200 p-2 w-full items-center mx-5 rounded-full">
+                <HiOutlineMagnifyingGlass/>
+                <input type="text" placeholder="Search Games" className="px-2 bg-transparent outline-none"/>
+            </div>
+            <div>
+                {toggle ? <HiMoon className='text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer' onClick={() => setToggle(!toggle)}/> : <HiSun className='text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer' onClick={() => setToggle(!toggle)}/>}
+            </div>
+        </div>
     )
 }
 
