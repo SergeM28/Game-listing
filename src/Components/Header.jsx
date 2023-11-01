52a55b3ac3eb9e12ce92e799c48fac3a90056822
serge.muradian@gmail.com <serge.muradian@gmail.com>
2023-11-01 11:40:03 +0300

    Create responsive layout

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import { ThemeContext } from '../Context/ThemeContext';
 
 const Header = () => {
     const [toggle, setToggle] = useState(false);
-    const {theme, setTheme} = useContext(ThemeContext);
+    const { theme, setTheme } = useContext(ThemeContext);
 
     useEffect(() => {
         console.log("Theme", theme)
@@ -13,13 +13,15 @@ const Header = () => {
 
     return (
         <div className="flex items-center p-3">
-            <img src={logo} width={60} height={60}/>
+            <img src={logo} width={60} height={60} />
             <div className="flex bg-slate-200 p-2 w-full items-center mx-5 rounded-full">
-                <HiOutlineMagnifyingGlass/>
-                <input type="text" placeholder="Search Games" className="px-2 bg-transparent outline-none"/>
+                <HiOutlineMagnifyingGlass />
+                <input type="text" placeholder="Search Games" className="px-2 bg-transparent outline-none" />
             </div>
             <div>
-                {theme === "light" ? <HiMoon className='text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer' onClick={() => setTheme("dark")}/> : <HiSun className='text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer' onClick={() => setTheme("light")}/>}
+                {theme === "light" ? (
+                    <HiMoon className='text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer' onClick={() => { setTheme("dark"); localStorage.setItem("theme", "dark") }} />) : (
+                    <HiSun className='text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer' onClick={() => { setTheme("light"); localStorage.setItem("theme", "light") }} /> )}
             </div>
         </div>
     )
