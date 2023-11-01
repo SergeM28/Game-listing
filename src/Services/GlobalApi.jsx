343848db4563f354470491188ae8d722db9e7359
serge.muradian@gmail.com <serge.muradian@gmail.com>
2023-11-01 12:18:06 +0300

    Fetch Data from API

---
+++ src/Services/GlobalApi.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const key = "55bc55c6a3ad46a0982a296c9a6a7792";
+const axiosCreate = axios.create({
+    baseURL: 'https://api.rawg.io/api'
+})
+
+const getGenreList = axiosCreate.get('/genres?key='+key);
+
+export default {
+    getGenreList
+}
